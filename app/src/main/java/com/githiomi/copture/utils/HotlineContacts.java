0f00bc7aa260c38032adde943333a1667b6b097b
githiomi <daniel.githiomi@gmail.com>
2024-07-17 21:19:41 +0400

    Feat: Refactored enum to use the Hotline Class

--- app/src/main/java/com/githiomi/copture/utils/HotlineContacts.java
+++ app/src/main/java/com/githiomi/copture/utils/HotlineContacts.java
@@ -1,37 +1,36 @@
 package com.githiomi.copture.utils;
 
+import com.githiomi.copture.models.Hotline;
+
 public enum HotlineContacts {
 
     // Police Stations
-    DIRECT("Police Direct Line", 999L),
-    CID("Central CID", 2302081212L),
-    POLICE_INFO("Police Information Services", 2080034L),
-    NORTH("North Police Headquarters",	2649709L),
-    SOUTH("South Police Headquarters",	6277216L),
-    CENTRAL("Central Police Headquarters",	6765116L),
-    EAST("East Police Headquarters",	4130944L),
-    WEST("East Police Headquarters",	4661764L),
+    DIRECT(new Hotline("Police Direct Line", 999L)),
+    CID(new Hotline("Central CID", 2302081212L)),
+    POLICE_INFO(new Hotline("Police Information Services", 2080034L)),
+    NORTH(new Hotline("North Police Headquarters", 2649709L)),
+    SOUTH(new Hotline("South Police Headquarters", 6277216L)),
+    CENTRAL(new Hotline("Central Police Headquarters", 6765116L)),
+    EAST(new Hotline("East Police Headquarters", 4130944L)),
+    WEST(new Hotline("East Police Headquarters", 4661764L)),
 
     // Lawyers
-    SUPREME("Mauritius Supreme Court", 3442L),
-    MAGISTRATE("Mauritius Magistrate Court", 159L),
+    SUPREME(new Hotline("Mauritius Supreme Court", 3442L)),
+    MAGISTRATE(new Hotline("Mauritius Magistrate Court", 159L)),
 
     // Medical Centers
-    SAMU("SAMU Ambulances", 114L),
-    C_CARE("C-Care Hospital", 43674586L),
-    SSR("Sir Seewoosagur Ramgoolam Hospital", 999L),
+    SAMU(new Hotline("SAMU Ambulances", 114L)),
+    C_CARE(new Hotline("C-Care Hospital", 43674586L)),
+    SSR(new Hotline("Sir Seewoosagur Ramgoolam Hospital", 999L)),
 
     // Fire Fighters
-    FIRE_SERVICES_1("Fire fighter Services 1", 995L),
-    FIRE_SERVICES_2("Fire fighter Services 2", 115L),
-    RAPID_RESPONSE("Fire Rapid Response", 4543L)
-    ;
+    FIRE_SERVICES_1(new Hotline("Fire fighter Services 1", 995L)),
+    FIRE_SERVICES_2(new Hotline("Fire fighter Services 2", 115L)),
+    RAPID_RESPONSE(new Hotline("Fire Rapid Response", 4543L));
 
-    private final String hotlineDescription;
-    private final Long hotlineNumber;
+    private final Hotline hotline;
 
-    HotlineContacts(String hotlineDescription, Long hotlineNumber) {
-        this.hotlineDescription = hotlineDescription;
-        this.hotlineNumber = hotlineNumber;
+    HotlineContacts(Hotline hotline) {
+        this.hotline = hotline;
     }
 }
