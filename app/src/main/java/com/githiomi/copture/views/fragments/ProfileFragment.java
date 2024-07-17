3996338bdfc3ea4a5470d098a1369a1dc2a5f181
githiomi <daniel.githiomi@gmail.com>
2024-07-17 19:58:28 +0400

    Chore: Minor changes to the app theme

--- app/src/main/java/com/githiomi/copture/views/fragments/ProfileFragment.java
+++ app/src/main/java/com/githiomi/copture/views/fragments/ProfileFragment.java
@@ -1,66 +1,67 @@
 package com.githiomi.copture.views.fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
-import com.githiomi.copture.R;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.githiomi.copture.databinding.FragmentProfileBinding;
+import com.githiomi.copture.utils.Animations;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class ProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    // Layout
+    Animations animations;
+    CircleImageView profilePicture;
+    LinearLayout badgeCards;
 
     public ProfileFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        return new ProfileFragment();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        FragmentProfileBinding fragmentProfileBinding = FragmentProfileBinding.inflate(inflater, container, false);
+
+        // Init animations
+        this.animations = new Animations(getContext());
+
+        // Inflate views
+        inflateViews(fragmentProfileBinding);
+
+        // Attach animations
+        attachAnimations();
+
+        return fragmentProfileBinding.getRoot();
+    }
+
+    /**
+     * Method to attach animations to the layout
+     *
+     * @param root The FragmentBinding ref to the root layout
+     */
+    private void inflateViews(FragmentProfileBinding root) {
+        this.profilePicture = root.IVProfilePicture;
+        this.badgeCards = root.LLBadgeCards;
+    }
+
+    private void attachAnimations() {
+        this.profilePicture.setAnimation(this.animations.getFromTopAnimation());
+        this.badgeCards.setAnimation(this.animations.getFromBottomAnimation());
     }
 }
