a6045b83e801bad39118d0a362cb1e925757f2c1
githiomi <daniel.githiomi@gmail.com>
2024-07-19 15:14:23 +0400

    Feat: Refactored primary button to include state

--- app/src/main/java/com/githiomi/copture/views/fragments/CreateFragment.java
+++ app/src/main/java/com/githiomi/copture/views/fragments/CreateFragment.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -30,6 +31,7 @@ public class CreateFragment extends Fragment implements RecyclerViewItemClickLis
     Animations animations;
     LottieAnimationView scanAnimationView;
     RecyclerView createNewTicketRecyclerView;
+    AppCompatButton createNewTicketButton;
 
     public CreateFragment() {
         // Required empty public constructor
